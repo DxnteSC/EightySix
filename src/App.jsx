b5f423fca211d45b31707672b171bef0c4416a3f
Dante <DanteS_C@outlook.com>
2024-03-15 18:34:29 +0000

    set-up

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { Flex, Heading } from '@chakra-ui/react'
+
+
+const App = () => {
+  return (
+    <Flex justifyContent='center'>
+      <Heading >86</Heading>
+    </Flex>
+  )
+}
+
+export default App
